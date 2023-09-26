5fd4693c411ce71de3be915b7de45a1087bc8dfc
OsqY <118946709+OsqY@users.noreply.github.com>
2023-09-26 15:25:55 -0600

    Initial commit

---
+++ src/main/java/com/app/apptareas/ApptareasApplication.java
@@ -0,0 +1,13 @@
+package com.app.apptareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApptareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApptareasApplication.class, args);
+	}
+
+}
